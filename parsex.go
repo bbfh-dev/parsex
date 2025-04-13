2bd349f37c08c3bf6dd9d5d29e59d18c5862a05b
BubbleFish <daforsastudia@gmail.com>
2025-04-13 16:38:30 +0300

    feat: Initial Program struct

--- parsex.go
+++ parsex.go
@@ -1,1 +1,17 @@
 package parsex
+
+type Program struct {
+	// Pointer to a [var ... struct{}] containing all program options.
+	//
+	// All input flags will be stored in the struct,
+	// --help menu will be automatically generated.
+	//
+	// Use the following field tags:
+	//
+	// `alt:"<single letter alternative use>" desc:"<description of the option>`
+	Data any
+	// The name of the executable / command
+	Name string
+	// Will be displayed in the --help menu
+	Desc string
+}
