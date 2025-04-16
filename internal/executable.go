5928752ad438f98464e64f7e66bb3eb0c1fff10e
BubbleFish <daforsastudia@gmail.com>
2025-04-16 17:09:10 +0300

    feat: Create Executable{} & ContextExecutable{}

---
+++ internal/executable.go
@@ -0,0 +1,29 @@
+package internal
+
+// [Executable] is a function that can be called by [parsex.Program].
+// Usually it's a function pointing to your own program/command logic.
+type Executable func(args []string) error
+
+// An [Executable] with context such as program arguments.
+type ContextExecutable struct {
+	Function Executable
+	Args     []string
+}
+
+func NewContextExecutable(exec Executable) *ContextExecutable {
+	return &ContextExecutable{
+		Function: exec,
+		Args:     []string{},
+	}
+}
+
+// Saves the argument in the executable context
+func (executable *ContextExecutable) AddArg(arg string) *ContextExecutable {
+	executable.Args = append(executable.Args, arg)
+	return executable
+}
+
+// Runs the [Executable] with the stored arguments
+func (executable *ContextExecutable) Run() error {
+	return executable.Function(executable.Args)
+}
